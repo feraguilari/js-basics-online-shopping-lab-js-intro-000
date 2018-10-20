87d313568713c32b0fa56df09a63a0760543d805
Fernando Aguilar Islas <f.aguislas@gmail.com>
2018-10-20 23:58:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function viewCart() {
       return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
     }else{
       let lastItem = array.pop();
-      return `In your cart, you have ${array.join(', ')}, and ${lastItem}.`
+      return `In your cart, you have ${array}, and ${lastItem}.`
     }
 }
 
