e35e6a5632c821d682115c6d5b7ba24061eaa0de
Fernando Aguilar Islas <f.aguislas@gmail.com>
2018-10-20 23:47:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ function setCart(c) {
 function addToCart(item) {
   var price = Math.floor(Math.random() * 100) + 1;
   console.log(price)
-  cart.push({[item]:price})
+  var item_object = {};
+  item_object[item] = price
+  cart.push(item_object)
  return `${item} has been added to your cart.`
 }
 
