20a5dcef2637a1f55da5ba664f59f707876a19db
Fernando Aguilar Islas <f.aguislas@gmail.com>
2018-10-21 00:19:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,10 +55,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (!cardNumber) {
-    return `Sorry, we don't have a credit card on file for you.`;
-  } else {
-   return `Your total cost is $${total()}, which will be charged to the card ${creditCard}.`;
-    cart = [];
-  }
+  if (cardNumber){
+  let grandTotal = total();
+  cart =[];
+  return`Your total cost is $${grandTotal}, which will be charged to the card ${cardNumber}.`;
+} else {
+  return "Sorry, we don't have a credit card on file for you.";
+}
 }
